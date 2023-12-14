8a2ba59c467994ace579d69cef600aaa80295c2b
พลพัต ทรวงหิรัญ <rocket@Macintosh.local>
2023-12-14 15:50:05 +0700

    First Commit Fixes Package Error

---
+++ src/components/Banner.js
@@ -0,0 +1,81 @@
+import { useState, useEffect } from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import { ArrowRightCircle } from "react-bootstrap-icons";
+import headerImg from "../assets/img/header-img.svg";
+
+export const Banner = () => {
+  const [loopNum, setLoopNum] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const toRotate = [
+    "Web Developer",
+    "Tester",
+    "PHP Developer",
+    "Front-End Developer",
+    "Back-End Developer",
+  ];
+  const [text, setText] = useState("");
+  const [delta, setDelta] = useState(300 - Math.random() * 100);
+  const period = 2000;
+
+  useEffect(() => {
+    let ticker = setInterval(() => {
+      tick();
+    }, delta);
+
+    return () => {
+      clearInterval(ticker);
+    };
+  }, [text]);
+
+  const tick = () => {
+    let i = loopNum % toRotate.length;
+    let fullText = toRotate[i];
+    let updateText = isDeleting
+      ? fullText.substring(0, text.length - 1)
+      : fullText.substring(0, text.length + 1);
+
+    setText(updateText);
+
+    if (isDeleting) {
+      setDelta((prevDelta) => prevDelta / 2);
+    }
+
+    if (!isDeleting && updateText === fullText) {
+      setIsDeleting(true);
+      setDelta(period);
+    } else if (isDeleting && updateText === "") {
+      setIsDeleting(false);
+      setLoopNum(loopNum + 1);
+      setDelta(500);
+    }
+  };
+
+  return (
+    <section className="banner" id="home">
+      <Container>
+        <Row className="align-items-center">
+          <Col xs={12} md={6} xl={7}>
+            <span className="tagline">Welcome to my Portfolio</span>
+            <h1>
+              {`Hi I'm Rocket`}
+              <span className="wrap"> {text}</span>
+            </h1>
+            <p>
+              Hi, I'm Phonlapat Suanghiran, also known as Rocket. I'm a
+              first-year transfer student in the Bachelor's Degree program for
+              Electrical Engineering and Software Engineering at RMUTL Chiang
+              Mai. I've been working on some projects related to my studies and
+              would love for you to check them out.
+            </p>
+            <button onClick={() => console.log("connnect")}>
+              Let's connect <ArrowRightCircle size={25} />
+            </button>
+          </Col>
+          <Col xs={12} md={6} xl={5}>
+            <img src={headerImg} alt="Header Img" />
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
---
+++ src/components/Projects.js
@@ -0,0 +1,87 @@
+import { Col, Row, Container, Tab, Nav } from "react-bootstrap";
+import { ProjectCard } from "./ProjectCard";
+import colorSharp2 from "../assets/img/color-sharp2.png";
+import projImg1 from "../assets/img/project-img1.png";
+import projImg2 from "../assets/img/project-img2.png";
+import projImg3 from "../assets/img/project-img3.png";
+
+export const Projects = () => {
+
+    const projects = [
+        {
+            title: "Qhunter",
+            description: "PHP Developer & Database Management",
+            imgUrl: projImg1,
+        },
+        {
+            title: "Qhunter",
+            description: "PHP Developer & Database Management",
+            imgUrl: projImg2,
+        },
+        {
+            title: "Qhunter",
+            description: "PHP Developer & Database Management",
+            imgUrl: projImg3,
+        },
+        {
+            title: "Qhunter",
+            description: "PHP Developer & Database Management",
+            imgUrl: projImg1,
+        },
+        {
+            title: "Qhunter",
+            description: "PHP Developer & Database Management",
+            imgUrl: projImg2,
+        },
+        {
+            title: "Qhunter",
+            description: "PHP Developer & Database Management",
+            imgUrl: projImg3,
+        },
+
+    ];
+    return(
+        <section className="project" id="project">
+            <Container>
+                <Row>
+                    <Col>
+                        <h2>Projects</h2>
+                        <p>This is a workplace project resulting from coding and training.</p>
+                        <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                        <Nav variant="pills" className="nav-pills mb-5 justify-content align-items-center" id="pills-tab">
+                            <Nav.Item>
+                                <Nav.Link eventKey="first">Tab One</Nav.Link>
+                            </Nav.Item>
+                        <Nav.Item>
+                                <Nav.Link eventKey="second">Tab Two</Nav.Link>
+                            </Nav.Item>
+                        <Nav.Item>
+                                <Nav.Link eventKey="third" disabled>Disabled</Nav.Link>
+                            </Nav.Item>
+                        </Nav>
+                        <Tab.Content>
+                        <Tab.Pane eventKey="first">
+                            <Row>
+                                {
+                                    projects.map((project, index) => {
+                                        return (
+                                            <ProjectCard
+                                                key={index}
+                                                {...project}
+                                                />
+                                        )
+                                    })
+                                }
+                            </Row>
+                        </Tab.Pane>
+                            <Tab.Pane eventKey="second">Loren Ipsum</Tab.Pane>
+                            <Tab.Pane eventKey="third">Loren Ipsum</Tab.Pane>
+                        </Tab.Content>
+                    </Tab.Container> 
+                    </Col>
+                </Row>
+            </Container>
+            <img className="background-image-right" src={colorSharp2}></img>
+        </section>
+    )
+}
